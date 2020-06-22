b41ce846b28daaba86fba9029d6b25f079bcbcd6
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-22 23:06:05 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,7 +3,7 @@ require "pry"
 def using_while(phrase)
   levitation_force = 6 
   while levitation_force < 10
-  puts "wingardium Leviosa"
+  puts "Wingardium Leviosa"
   levitation_force += 1
   end
  
