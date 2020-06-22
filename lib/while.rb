fc3ba0ef2b16cb7ba5a37d29dbde42a6c50d3793
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-22 23:00:38 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
-def using_while
-  levitation_force = 6
+def using_while(phrase)
+  levitation_force = 6 
+while levitation_force <=
+puts "wingardium Leviosa"
+levitation_force+=1
 	
-  #your code here
+
 end
 
 
